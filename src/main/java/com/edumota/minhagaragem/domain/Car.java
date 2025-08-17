e2ed59184c4f529a8a58e9312b521ba0ac8e9b35
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-17 18:43:31 -0300

    feat: add Cars entity

---
+++ src/main/java/com/edumota/minhagaragem/domain/Car.java
@@ -0,0 +1,38 @@
+package com.edumota.minhagaragem.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int year;
+
+    private String colour;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+}
