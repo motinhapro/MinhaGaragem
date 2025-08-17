5e753ca3fae6f984940c4b941c5992378ccfa10e
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-17 18:27:04 -0300

    First commit

---
+++ src/main/java/com/edumota/minhagaragem/MinhaGaragemApplication.java
@@ -0,0 +1,13 @@
+package com.edumota.minhagaragem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaGaragemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinhaGaragemApplication.class, args);
+    }
+
+}
