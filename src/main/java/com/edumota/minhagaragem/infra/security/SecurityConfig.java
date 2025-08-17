8385c5d2e1ef78da571f09c9b44991ef544ebb4c
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-17 20:46:30 -0300

    feat: add BCryptPasswordEncoder for secure password hashing

--- src/main/java/com/edumota/minhagaragem/infra/security/SecurityConfig.java
+++ src/main/java/com/edumota/minhagaragem/infra/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -51,5 +52,10 @@ public class SecurityConfig {
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
