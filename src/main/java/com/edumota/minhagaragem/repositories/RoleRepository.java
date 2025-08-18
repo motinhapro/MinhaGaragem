0f9a68cd6fd28a3d1e1d354ff8fe482474babd3b
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-17 21:04:33 -0300

    feat: add role repository

---
+++ src/main/java/com/edumota/minhagaragem/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.edumota.minhagaragem.repositories;
+
+import com.edumota.minhagaragem.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
