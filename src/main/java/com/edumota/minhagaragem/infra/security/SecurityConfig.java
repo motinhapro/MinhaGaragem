cfd827b378bfbc30aec237a4a6abaaee00018441
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-18 18:39:24 -0300

    feat: add permission for all users to register endpoint

--- src/main/java/com/edumota/minhagaragem/infra/security/SecurityConfig.java
+++ src/main/java/com/edumota/minhagaragem/infra/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
