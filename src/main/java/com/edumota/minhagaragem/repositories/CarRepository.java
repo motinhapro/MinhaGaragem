ab5aa5c7ff534064712100ffd3fe1f0f3442c3d5
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-19 19:01:27 -0300

    feat: add find all cars by user id

--- src/main/java/com/edumota/minhagaragem/repositories/CarRepository.java
+++ src/main/java/com/edumota/minhagaragem/repositories/CarRepository.java
@@ -1,7 +1,12 @@
 package com.edumota.minhagaragem.repositories;
 
 import com.edumota.minhagaragem.domain.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+    Page<Car> findByUserId(UUID id, Pageable pageable);
 }
