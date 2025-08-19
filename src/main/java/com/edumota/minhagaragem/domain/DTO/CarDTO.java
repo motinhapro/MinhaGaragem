d663335a3532a656aa2de7b12cbef5ef09b71cc1
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-19 18:58:45 -0300

    feat: add constructor em CarDTO to transform a Car on CarDTO

--- src/main/java/com/edumota/minhagaragem/domain/DTO/CarDTO.java
+++ src/main/java/com/edumota/minhagaragem/domain/DTO/CarDTO.java
@@ -1,4 +1,10 @@
 package com.edumota.minhagaragem.domain.DTO;
 
+import com.edumota.minhagaragem.domain.Car;
+
 public record CarDTO(String model, String brand, int year, String colour) {
+
+    public CarDTO(Car car) {
+        this(car.getModel(), car.getBrand(), car.getYear(), car.getColour());
+    }
 }
