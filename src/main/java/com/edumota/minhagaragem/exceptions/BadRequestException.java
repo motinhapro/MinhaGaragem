ed15506dca8dc87d27bcaa92f494a83b1794415c
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-19 21:43:12 -0300

    feat: add custom bad request exception

---
+++ src/main/java/com/edumota/minhagaragem/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.edumota.minhagaragem.exceptions;
+
+public class BadRequestException extends RuntimeException {
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
