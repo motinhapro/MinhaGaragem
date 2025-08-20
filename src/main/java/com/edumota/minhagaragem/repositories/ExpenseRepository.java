4edb92b02c30e4c8022988ab1b825204a2a66909
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-19 21:07:56 -0300

    feat: add find by car

--- src/main/java/com/edumota/minhagaragem/repositories/ExpenseRepository.java
+++ src/main/java/com/edumota/minhagaragem/repositories/ExpenseRepository.java
@@ -1,7 +1,12 @@
 package com.edumota.minhagaragem.repositories;
 
+import com.edumota.minhagaragem.domain.Car;
+import com.edumota.minhagaragem.domain.DTO.expense.ExpenseDTO;
 import com.edumota.minhagaragem.domain.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    Page<Expense> findByCar(Car car, Pageable pageable);
 }
