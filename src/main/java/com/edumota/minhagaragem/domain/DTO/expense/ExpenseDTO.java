66750154c568e23e3d19f9a6ca4f253d8616adea
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-08-19 21:07:00 -0300

    feat: add constructor to transform an expense in a expenseDTO

--- src/main/java/com/edumota/minhagaragem/domain/DTO/expense/ExpenseDTO.java
+++ src/main/java/com/edumota/minhagaragem/domain/DTO/expense/ExpenseDTO.java
@@ -1,9 +1,14 @@
 package com.edumota.minhagaragem.domain.DTO.expense;
 
 import com.edumota.minhagaragem.domain.Car;
+import com.edumota.minhagaragem.domain.Expense;
 import com.edumota.minhagaragem.domain.enums.ExpenseType;
 
 import java.math.BigDecimal;
 
 public record ExpenseDTO(BigDecimal price, String description, ExpenseType expenseType, Car car) {
+
+    public ExpenseDTO(Expense expense) {
+        this(expense.getPrice(), expense.getDescription(), expense.getExpenseType(), expense.getCar());
+    }
 }
