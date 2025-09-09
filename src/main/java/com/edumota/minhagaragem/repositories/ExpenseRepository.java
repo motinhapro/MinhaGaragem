30d3ec7bdce8dd194bd5fe928a397025d6aa3227
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-09 20:28:42 -0300

    fix: wrong name on query

--- src/main/java/com/edumota/minhagaragem/repositories/ExpenseRepository.java
+++ src/main/java/com/edumota/minhagaragem/repositories/ExpenseRepository.java
@@ -16,15 +16,14 @@ import java.util.UUID;
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     Page<Expense> findByCar(Car car, Pageable pageable);
 
-    @Query("SELECT e FROM Expense where e.car.user.id = :userId")
-    Page<Expense> findByCarUserId(@Param("userId") UUID userId, Pageable pageable);
+    Page<Expense> findByCar_User_Id(@Param("userId") UUID userId, Pageable pageable);
 
     @Query("SELECT SUM(e.price) FROM Expense e WHERE e.car.user.id = :userId")
     BigDecimal findTotalSpendingByUserId(@Param("userId") UUID userId);
 
-    @Query("SELECT new com.edumota.minhagaragem.domain.DTO.finance.SpendingByCategoryDTO(e.type, SUM(e.price)) " +
-            "FROM Expense where e.car.user.id = :userId " +
-            "GROUP BY e.type " +
+    @Query("SELECT new com.edumota.minhagaragem.domain.DTO.finance.SpendingByCategoryDTO(e.expenseType, SUM(e.price)) " +
+            "FROM Expense e WHERE e.car.user.id = :userId " +
+            "GROUP BY e.expenseType " +
             "ORDER BY SUM(e.price) DESC")
     List<SpendingByCategoryDTO> findSpendingGroupedByType(@Param("userId") UUID userId);
 }
