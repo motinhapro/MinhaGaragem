f555eb3fdff04343861fb2c9f884e87c17abfb15
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-08 21:06:55 -0300

    feat: add a method in finance service to calculate all user expenses

--- src/main/java/com/edumota/minhagaragem/services/FinanceService.java
+++ src/main/java/com/edumota/minhagaragem/services/FinanceService.java
@@ -1,12 +1,26 @@
 package com.edumota.minhagaragem.services;
 
+import com.edumota.minhagaragem.domain.DTO.finance.TotalSpendingDTO;
 import com.edumota.minhagaragem.repositories.ExpenseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 public class FinanceService {
 
     private final ExpenseRepository expenseRepository;
+
+    public TotalSpendingDTO calculateTotalSpending(UUID userId) {
+        BigDecimal total = expenseRepository.findTotalSpendingByUserId(userId);
+
+        if(total == null) {
+            total = BigDecimal.ZERO;
+        }
+
+        return new TotalSpendingDTO(total);
+    }
 }
