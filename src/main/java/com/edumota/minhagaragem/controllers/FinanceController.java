1afaddd833309a4e087877420802d84a5e9b3bd1
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-13 18:45:49 -0300

    refactor: move general expense endpoint to ExpenseController

--- src/main/java/com/edumota/minhagaragem/controllers/FinanceController.java
+++ src/main/java/com/edumota/minhagaragem/controllers/FinanceController.java
@@ -1,13 +1,9 @@
 package com.edumota.minhagaragem.controllers;
 
-import com.edumota.minhagaragem.domain.DTO.expense.ExpenseDTO;
 import com.edumota.minhagaragem.domain.DTO.finance.SpendingByCategoryDTO;
 import com.edumota.minhagaragem.domain.DTO.finance.TotalSpendingDTO;
-import com.edumota.minhagaragem.services.ExpenseService;
 import com.edumota.minhagaragem.services.FinanceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,13 +21,6 @@ import java.util.UUID;
 public class FinanceController {
 
     private final FinanceService financeService;
-
-    private final ExpenseService expenseService;
-
-    @GetMapping("/expenses")
-    public ResponseEntity<Page<ExpenseDTO>> findMyExpenses(Authentication authentication, Pageable pageable) {
-        return ResponseEntity.ok().body(expenseService.findMyExpenses(UUID.fromString(authentication.getName()), pageable));
-    }
 
     @GetMapping("/summary/total")
     public ResponseEntity<TotalSpendingDTO> getTotalSpending(
