d1977017cbd9b8919003a3db275f5bc3d2073c2a
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 17:27:11 -0300

    feat(sale): add SaleRepository

--- src/main/java/com/edumota/minhagaragem/repositories/SaleRepository.java
+++ src/main/java/com/edumota/minhagaragem/repositories/SaleRepository.java
@@ -2,6 +2,7 @@ package com.edumota.minhagaragem.repositories;
 
 import com.edumota.minhagaragem.domain.entities.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+public interface SaleRepository extends JpaRepository<Sale, Long>, JpaSpecificationExecutor<Sale> {
 }
