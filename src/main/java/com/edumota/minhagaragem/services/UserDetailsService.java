a2064eb52617812009a003af5142856077b66163
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 12:43:35 -0300

    feat: add UserDetailsService

---
+++ src/main/java/com/edumota/minhagaragem/services/UserDetailsService.java
@@ -0,0 +1,16 @@
+package com.edumota.minhagaragem.services;
+
+import com.edumota.minhagaragem.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: + username"));
+    }
+}
