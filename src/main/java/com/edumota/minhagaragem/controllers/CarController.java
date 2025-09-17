e17cdee59338d843525a05ce55b0dd6e2fb0378e
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 13:03:13 -0300

    refactor: Use @AuthenticationPrincipal in CarController methods

--- src/main/java/com/edumota/minhagaragem/controllers/CarController.java
+++ src/main/java/com/edumota/minhagaragem/controllers/CarController.java
@@ -5,7 +5,7 @@ import com.edumota.minhagaragem.domain.DTO.car.CarPostDTO;
 import com.edumota.minhagaragem.domain.DTO.car.CarUpdateDTO;
 import com.edumota.minhagaragem.domain.DTO.expense.ExpenseDTO;
 import com.edumota.minhagaragem.domain.DTO.expense.ExpensePostDTO;
-import com.edumota.minhagaragem.domain.DTO.expense.ExpenseUpdateDTO;
+import com.edumota.minhagaragem.domain.entities.User;
 import com.edumota.minhagaragem.services.CarService;
 import com.edumota.minhagaragem.services.ExpenseService;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +14,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 
 @RequiredArgsConstructor
@@ -32,39 +31,38 @@ public class CarController {
 
     @GetMapping
     public ResponseEntity<Page<CarDTO>> findMyCars(
-            Authentication authentication, Pageable pageable,
+            @AuthenticationPrincipal User userDetails, Pageable pageable,
             @RequestParam(required = false) String brand, @RequestParam(required = false) String model,
             @RequestParam(required = false) Integer minYear, @RequestParam(required = false) Integer maxYear,
-            @RequestParam(required = false)  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-            )
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate)
     {
-        return ResponseEntity.ok().body(carService.findMyCars(UUID.fromString(authentication.getName()), pageable, brand, model, minYear, maxYear, startDate, endDate));
+        return ResponseEntity.ok(carService.findMyCars(userDetails.getId(), pageable, brand, model, minYear, maxYear, startDate, endDate));
     }
 
     @PostMapping
-    public ResponseEntity<CarDTO> insert(@RequestBody CarPostDTO car, Authentication authentication) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(carService.insert(UUID.fromString(authentication.getName()), car));
+    public ResponseEntity<CarDTO> insert(@RequestBody CarPostDTO car, @AuthenticationPrincipal User userDetails) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(carService.insert(userDetails.getId(), car));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id, Authentication authentication) {
-        carService.delete(id, UUID.fromString(authentication.getName()));
+    public ResponseEntity<Void> delete(@PathVariable Long id, @AuthenticationPrincipal User userDetails) {
+        carService.delete(id, userDetails.getId());
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CarDTO> update(@PathVariable Long id, @RequestBody CarUpdateDTO car, Authentication authentication) {
-        return ResponseEntity.ok().body(carService.update(id, car, UUID.fromString(authentication.getName())));
+    public ResponseEntity<CarDTO> update(@PathVariable Long id, @RequestBody CarUpdateDTO car, @AuthenticationPrincipal User userDetails) {
+        return ResponseEntity.ok(carService.update(id, car, userDetails.getId()));
     }
 
-    @PostMapping("/{id}/expenses")
-    public ResponseEntity<ExpenseDTO> insertMyExpenseByCar(@RequestBody ExpensePostDTO newExpense,@PathVariable Long id, Authentication authentication) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(expenseService.insertMyExpenseByCar(newExpense, id, UUID.fromString(authentication.getName())));
+    @PostMapping("/{carId}/expenses")
+    public ResponseEntity<ExpenseDTO> insertMyExpenseByCar(@RequestBody ExpensePostDTO newExpense, @PathVariable Long carId, @AuthenticationPrincipal User userDetails) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(expenseService.insertMyExpenseByCar(newExpense, carId, userDetails.getId()));
     }
 
-    @GetMapping("/{id}/expenses")
-    public ResponseEntity<Page<ExpenseDTO>> findMyExpensesByCar(@PathVariable Long id, Authentication authentication, Pageable pageable) {
-        return ResponseEntity.ok().body(expenseService.findMyExpensesByCar(id, UUID.fromString(authentication.getName()), pageable));
+    @GetMapping("/{carId}/expenses")
+    public ResponseEntity<Page<ExpenseDTO>> findMyExpensesByCar(@PathVariable Long carId, @AuthenticationPrincipal User userDetails, Pageable pageable) {
+        return ResponseEntity.ok(expenseService.findMyExpensesByCar(carId, userDetails.getId(), pageable));
     }
 }
