28130c6622ecaf03fdee89a0ced270f14b7783d1
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 14:54:46 -0300

    feat: add JpaSpecifications to ClientRepository

--- src/main/java/com/edumota/minhagaragem/repositories/ClientRepository.java
+++ src/main/java/com/edumota/minhagaragem/repositories/ClientRepository.java
@@ -2,6 +2,7 @@ package com.edumota.minhagaragem.repositories;
 
 import com.edumota.minhagaragem.domain.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long>, JpaSpecificationExecutor<Client> {
 }
