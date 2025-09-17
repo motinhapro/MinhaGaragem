c9859c515366f2e5dee55ee04da36a03fad112d7
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 14:37:16 -0300

    feat: add mapping constructor to ClientDTO

--- src/main/java/com/edumota/minhagaragem/domain/DTO/client/ClientDTO.java
+++ src/main/java/com/edumota/minhagaragem/domain/DTO/client/ClientDTO.java
@@ -1,4 +1,10 @@
 package com.edumota.minhagaragem.domain.DTO.client;
 
+import com.edumota.minhagaragem.domain.entities.Client;
+
 public record ClientDTO(String name, String number) {
+
+    public ClientDTO(Client client) {
+        this(client.getName(), client.getNumber());
+    }
 }
