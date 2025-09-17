1de9cf26a92a9ed41af7004b5300167ddd618315
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 13:05:10 -0300

    refactor: Use @AuthenticationPrincipal in ExpenseController methods

--- src/main/java/com/edumota/minhagaragem/controllers/ExpenseController.java
+++ src/main/java/com/edumota/minhagaragem/controllers/ExpenseController.java
@@ -2,6 +2,7 @@ package com.edumota.minhagaragem.controllers;
 
 import com.edumota.minhagaragem.domain.DTO.expense.ExpenseDTO;
 import com.edumota.minhagaragem.domain.DTO.expense.ExpenseUpdateDTO;
+import com.edumota.minhagaragem.domain.entities.User;
 import com.edumota.minhagaragem.domain.enums.ExpenseType;
 import com.edumota.minhagaragem.services.ExpenseService;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,24 +24,22 @@ public class ExpenseController {
 
     @GetMapping()
     public ResponseEntity<Page<ExpenseDTO>> findMyExpenses(
-            Authentication authentication, Pageable pageable,
+            @AuthenticationPrincipal User userDetails, Pageable pageable,
             @RequestParam(required = false) Long carId,
-            @RequestParam(required = false)ExpenseType type,
+            @RequestParam(required = false) ExpenseType type,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ) {
-        return ResponseEntity.ok().body(expenseService.findMyExpenses(UUID.fromString(authentication.getName()), pageable, carId, type, startDate, endDate));
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        return ResponseEntity.ok(expenseService.findMyExpenses(userDetails.getId(), pageable, carId, type, startDate, endDate));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ExpenseDTO> updateExpense(Authentication authentication,  @PathVariable Long id, @RequestBody ExpenseUpdateDTO dto) {
-        return ResponseEntity.ok().body(expenseService.updateMyExpense(UUID.fromString(authentication.getName()), id, dto));
+    public ResponseEntity<ExpenseDTO> updateExpense(@AuthenticationPrincipal User userDetails, @PathVariable Long id, @RequestBody ExpenseUpdateDTO dto) {
+        return ResponseEntity.ok(expenseService.updateMyExpense(userDetails.getId(), id, dto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteExpense(Authentication authentication, @PathVariable Long id) {
-        expenseService.deleteMyExpense(UUID.fromString(authentication.getName()), id);
+    public ResponseEntity<Void> deleteExpense(@AuthenticationPrincipal User userDetails, @PathVariable Long id) {
+        expenseService.deleteMyExpense(userDetails.getId(), id);
         return ResponseEntity.noContent().build();
     }
-
 }
