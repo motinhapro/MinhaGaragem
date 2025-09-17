b798967478f2a7be91db5583856ae0a4b6430491
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-09-17 16:53:08 -0300

    refactor(sale): using UUID for security

--- src/main/java/com/edumota/minhagaragem/domain/entities/Sale.java
+++ src/main/java/com/edumota/minhagaragem/domain/entities/Sale.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -18,8 +19,8 @@ import java.time.Instant;
 public class Sale {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(nullable = false)
     private BigDecimal price;
